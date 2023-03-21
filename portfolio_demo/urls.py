bdbba5995886ce21b660146c8106393b66d65fd4
Lufuno Maphwanya <lufunom@Thes-MacBook-Pro.local>
2023-03-21 13:55:16 +0200

    Adds user account and landing UI.

--- portfolio_demo/urls.py
+++ portfolio_demo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from useraccount.views import landing_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page_view, name="home"),
+
 ]
