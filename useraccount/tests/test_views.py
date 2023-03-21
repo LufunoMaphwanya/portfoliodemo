554b860fb5e70a66c47d66c2919cc94ffd3a1e3f
Lufuno Maphwanya <lufunom@Thes-MacBook-Pro.local>
2023-03-21 22:54:32 +0200

    Set up view tests.

--- useraccount/tests/test_views.py
+++ useraccount/tests/test_views.py
@@ -1,16 +1,31 @@
-#
-# from django.test import TestCase
-#
-# # Create your tests here.
-# class YourTestClaeess(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         print("setUpTestData: Run once to set up non-modified data for all class methods.")
-#         pass
-#
-#     def setUp(self):
-#         print("setUp: Run once for every test method to setup clean data.")
-#         pass
-#
-#     def test_false_is_false(self):
-#         pass
+from django.test import TestCase
+from django.urls import reverse
+
+from useraccount.models import UserAccount
+
+class AccountListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        for i in range(0, 20):
+            UserAccount.objects.create(
+                email=f"user{i}@testmail.com",
+                username=f"username{i}",
+                first_name=f"person-{i}",
+                last_name=f"person-{i}",
+                phone_number=f"surname-{i}",
+                address_x=f"400",
+                address_y=f"-200",
+            )
+    def test_redirects_to_login_protected_pages(self):
+
+        protected_pages = ['network', 'profile']
+
+        response1 = self.client.get(reverse(protected_pages[0]))
+        response2 = self.client.get(reverse(protected_pages[1]))
+
+        self.assertEqual(
+            (response1.status_code, response2.status_code),
+            (302, 302) #redirects to login
+        )
+
