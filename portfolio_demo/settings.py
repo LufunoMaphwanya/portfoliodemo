d5b8855459cf25c8a57ba441e52ab25615495c1d
Lufuno Maphwanya <lufunom@Thes-MacBook-Pro.local>
2023-03-21 15:11:21 +0200

    Add custom user model to settings.py to override django auth.

--- portfolio_demo/settings.py
+++ portfolio_demo/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'useraccount',
 ]
 
+AUTH_USER_MODEL = "useraccount.UserAccount"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
